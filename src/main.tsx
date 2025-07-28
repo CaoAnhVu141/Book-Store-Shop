e981bf4312ff5e897222c538eb0573b50f7eaee2
caoanhvu <caovu5541@gmail.com>
2025-07-29 00:31:51 +0700

    commit push code

--- src/main.tsx
+++ src/main.tsx
@@ -6,20 +6,21 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import BookPage from 'pages/client/book.tsx';
-import AboutPage from './pages/client/about.tsx';
-import LoginPage from './pages/client/auth/login.tsx';
-import RegisterPage from './pages/client/auth/register.tsx';
+import AboutPage from 'pages/client/about.tsx';
+import LoginPage from 'pages/client/auth/login.tsx';
+import RegisterPage from 'pages/client/auth/register.tsx';
 import './style/global.scss'
-import ManagerUser from './pages/admin/manager.user.tsx';
-import { ProConfigProvider } from '@ant-design/pro-components';
 import enUS from 'antd/locale/en_US';
 import { ConfigProvider } from 'antd';
+import HomePage from 'pages/client/home.tsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
+      { index: true,
+        element: <HomePage /> },
       {
         path: "/book",
         element: <BookPage />,
@@ -27,11 +28,7 @@ const router = createBrowserRouter([
       {
         path: "/about",
         element: <AboutPage />,
-      },
-      {
-        path: "/admin/user",
-        element: <ManagerUser />,
-      },
+      }
     ]
   },
   {
@@ -48,7 +45,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <Layout /> */}
     <ConfigProvider locale={enUS}>
-        <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </ConfigProvider>
   </StrictMode>,
 )
--- src/pages/client/auth/register.tsx
+++ src/pages/client/auth/register.tsx
@@ -1,7 +1,90 @@
+import type { FormProps } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
+import React from 'react';
 
 const RegisterPage = () => {
-    return(
-        <div>RegisterPage</div>
+
+    interface IUserRegister {
+        name: string,
+        email: string,
+        password: string,
+    }
+
+    const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+        console.log(values);
+    };
+
+    return (
+        <>
+            <div className='all-form-register' style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+                backgroundColor: "#f0f2f5",
+                margin: 0, padding: 0
+            }}>
+                <div className='form-register' style={{
+                    background: "white",
+                    padding: "40px 10px",
+                    borderRadius: "12px",
+                    boxShadow: "0 8px 24px rgba(0, 0, 0, 0.12)",
+                    width: "100%",
+                    maxWidth: "500px",
+                    justifyContent: "center"
+                }}>
+                    <Form
+                        name="basic"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        style={{ maxWidth: 600 }}
+                        initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                        // onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                    >
+                        <Form.Item<FieldType>
+                            label="Name"
+                            name="name"
+                            rules={[{ required: true, message: 'Please input your name!' }]}
+                        >
+                            <Input style={{
+                                width: "250px",
+                                padding: "5px 5px"
+                            }} />
+                        </Form.Item>
+
+                        <Form.Item<FieldType>
+                            label="Email"
+                            name="email"
+                            rules={[{ required: true, message: 'Please input your email!' }]}
+                        >
+                            <Input style={{
+                                width: "250px",
+                                padding: "5px 5px"
+                            }} />
+                        </Form.Item>
+
+                        <Form.Item<FieldType>
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password style={{
+                                width: "250px",
+                                padding: "5px 5px"
+                            }} />
+                        </Form.Item>
+
+                        <Form.Item label={null} style={{ display: "flex", justifyContent: "center", }}>
+                            <Button type="primary" htmlType="submit">
+                                Đăng kí
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            </div>
+        </>
     )
 }
 export default RegisterPage;
